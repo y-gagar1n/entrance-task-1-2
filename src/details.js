b4cb9fba2651fd26e2e271066dc6e639ca4ec352
y-gagar1n <y.timofeyev@gmail.com>
2018-08-14 19:28:03 +0300

    fix balloon template

--- src/details.js
+++ src/details.js
@@ -27,7 +27,7 @@ export function getDetailsContentLayout(ymaps) {
         {% endif %}
     `,
     {
-      build: () => {
+      build: function() {
         BalloonContentLayout.superclass.build.call(this);
 
         const { details } = this.getData().object.properties;
@@ -43,7 +43,7 @@ export function getDetailsContentLayout(ymaps) {
         }
       },
 
-      clear: () => {
+      clear: function() {
         if (this.connectionChart) {
           this.connectionChart.destroy();
         }
